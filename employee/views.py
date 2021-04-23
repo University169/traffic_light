133846df04ea5a832c845c69d63daa2e694e661c
University169 <dmklimkov@gmail.com>
2021-04-23 15:51:49 +0200

    tree expand-collapse

--- employee/views.py
+++ employee/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
+from .models import Employee
 # Create your views here.
+
+
+def show_employees(request):
+    global employees
+    employees = Employee.objects.all().filter(level__lte=5)
+    return render(request, "employees.html", {'employees': employees})
