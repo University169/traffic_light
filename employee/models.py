b0493510899abaa448c86cf8b755889fea119622
University169 <dmklimkov@gmail.com>
2021-05-10 01:34:32 +0200

    new models

--- employee/models.py
+++ employee/models.py
@@ -5,14 +5,17 @@ from mptt.models import MPTTModel, TreeForeignKey
 
 
 class Subdivision(MPTTModel):
-    # вся структура подразделениq хранится здесь
+    # вся структура подразделений хранится здесь
     # у создаваемого элемента есть родитель из этой же модели (либо None)
     subdivision_name = models.CharField(max_length=200)
     date_added = models.DateTimeField(auto_now_add=True)
     parent = TreeForeignKey('self', on_delete=models.CASCADE,
-                            null=True, blank=True, related_name='children_subdivision')
+                            null=True, blank=True, related_name='children_sub')
     def __str__(self):
         return f'Podrazdelenie: {self.subdivision_name}'
+
+    #def all_positions_to_string(self):
+        #return ", ".join([Position.employment_position for employment_position in self.children_position.all()])
 
     class MPTTMeta:
         order_insertion_by = ['subdivision_name']
@@ -20,25 +23,25 @@ class Subdivision(MPTTModel):
 
 class Position(models.Model):
     # должности
-    # здесь нет иерархии, это просто перечень возможных должностей для того или иного подразделения
-    # создавая должность мы ей выбираем в качестве родителя подразделение из модели Subdivision
-    employment_position = models.CharField(max_length=200)
+    # здесь нет иерархии, это просто перечень возможных должностей
+    employment_position = models.CharField(max_length=200, null=True, blank=True)
     salary = models.IntegerField()
-    parent = TreeForeignKey(Subdivision, on_delete=models.CASCADE, related_name='children_position')
 
     def __str__(self):
-        return f'{self.parent}; Dolzhnost: {self.employment_position}'
+        return f'Dolzhnost: {self.employment_position}'
 
 
 class Persone(models.Model):
     # здесь нет иерархии, тут указываются личные данные человека
-    # а в качестве родителя указывается конкретная должность из модели Position
-    name = models.CharField(max_length=50)
+    # а в качестве внешних ключей указывается конкретная должность из модели Position
+    # и конкретное подразделение
+    name = models.CharField(max_length=50, null=True, blank=True)
     tax_id_inn = models.CharField(max_length=12, unique=True)
-    # employment_position = models.CharField(max_length=200)
     employment_start_date = models.DateTimeField(auto_now_add=False)
-    # date_added = models.DateTimeField(auto_now_add=True)
-    parent = models.ForeignKey(Position, on_delete=models.CASCADE, null=True, blank=True, related_name='children_persone')
+    employee_position = models.ForeignKey(Position, on_delete=models.CASCADE, null=True,
+                               blank=True, related_name='children_position')
+    employee_subdivision = TreeForeignKey(Subdivision, on_delete=models.CASCADE, null=True,
+                               blank=True, related_name='children_subdivision')
 
     def __str__(self):
         return str(self.id) + ' Name: ' + self.name
--- employee/views.py
+++ employee/views.py
@@ -1,10 +1,19 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView, CreateView, DeleteView
 
 from .models import Subdivision, Position, Persone
 # Create your views here.
 
 
+template_name = "employee/subdivisions.html"
 def show_subdivisions(request):
     global subdivisions
+    global positions
     subdivisions = Subdivision.objects.all().filter(level__lte=5)
-    return render(request, "subdivisions.html", {'subdivisions': subdivisions})
+    positions = Position.objects.all()
+    # print('-'*30)
+    # print(subdivisions)
+    # print('=' * 30)
+    # print(positions)
+    # print('-' * 30)
+    return render(request, "subdivisions.html", {'subdivisions': subdivisions, 'positions': positions})
