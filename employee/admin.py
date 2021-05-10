57af6d36c2ace2827909734964714d24b91290b2
University169 <dmklimkov@gmail.com>
2021-05-10 16:56:32 +0200

    good working

--- employee/admin.py
+++ employee/admin.py
@@ -2,9 +2,10 @@ from django.contrib import admin
 
 # Register your models here.
 from . models import Persone, Subdivision, Position
+from mptt.admin import MPTTModelAdmin
 
 
 admin.site.register(Persone)
-admin.site.register(Subdivision)
+admin.site.register(Subdivision, MPTTModelAdmin)
 admin.site.register(Position)
 
--- employee/views.py
+++ employee/views.py
@@ -5,15 +5,10 @@ from .models import Subdivision, Position, Persone
 # Create your views here.
 
 
-template_name = "employee/subdivisions.html"
 def show_subdivisions(request):
-    global subdivisions
-    global positions
-    subdivisions = Subdivision.objects.all().filter(level__lte=5)
+    global subdivisions, positions,persons
+    subdivisions = Subdivision.objects.all().prefetch_related('children_subdivision').filter(level__lte=5)
     positions = Position.objects.all()
-    # print('-'*30)
-    # print(subdivisions)
-    # print('=' * 30)
-    # print(positions)
-    # print('-' * 30)
-    return render(request, "subdivisions.html", {'subdivisions': subdivisions, 'positions': positions})
+    persons = Persone.objects.all()
+    context = {'subdivisions': subdivisions, 'positions': positions, 'persons': persons}
+    return render(request, "subdivisions.html", context)
